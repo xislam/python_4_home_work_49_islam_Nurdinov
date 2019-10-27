fb56136855d38d72e046b9d13c5750af6b34d475
islam <nurdinovislam4@gmail.com>
2019-10-27 16:47:30 +0600

    написа фотму для акаунта (forms.py) добал модели написал вибшки

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import login_view, logout_view
+from .views import login_view, logout_view, register_view, user_activate_view
 
 app_name = 'accounts'
 
@@ -8,4 +8,6 @@ urlpatterns = [
 
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
+    path('activate/<token>/', user_activate_view, name='user_activate')
 ]
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,31 +1,72 @@
-from django.contrib.auth import login, authenticate, logout
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from accounts.models import Token
+from main.settings import HOST_NAME
+
+from accounts.forms import SignUpForm
 
 
 def login_view(request):
-
     context = {}
-
     if request.method == 'POST':
-
         username = request.POST.get('username')
-
         password = request.POST.get('password')
-
         user = authenticate(request, username=username, password=password)
-
         if user is not None:
             login(request, user)
-            return redirect('webapp:index')
-
+            next_url = request.GET.get('next', '')
+            if next_url:
+                return redirect(next_url)
+            return redirect('index')
         else:
             context['has_error'] = True
-
     return render(request, 'login.html', context=context)
 
 
 def logout_view(request):
-
     logout(request)
+    return redirect('index')
 
-    return redirect('webapp:index')
+
+def register_view(request):
+    if request.method == 'GET':
+        form = SignUpForm()
+        return render(request, 'register.html', context={'form': form})
+    elif request.method == 'POST':
+        form = SignUpForm(data=request.POST)
+        if form.is_valid():
+            user = User(
+                username=form.cleaned_data.get('username'),
+                email=form.cleaned_data.get('email'),
+                is_active=False
+            )
+            user.set_password(form.cleaned_data.get('password'))
+            user.save()
+
+            token = Token.objects.create(user=user)
+
+            activation_url = HOST_NAME + reverse('accounts:user_activate', kwargs={'token': token})
+            print(activation_url)
+            try:
+                user.email_user(
+                    'Вы зарегистрировались на сайте localhost:8000.',
+                    'Для активации перейдите по ссылке: ' + activation_url
+                )
+            except ConnectionRefusedError:
+                print('Could not send email. Server error.')
+
+            return redirect('index')
+        else:
+            return render(request, 'register.html', context={'form': form})
+
+
+def user_activate_view(request, token):
+    token = get_object_or_404(Token, token=token)
+    user = token.user
+    user.is_active = True
+    user.save()
+    login(request, user)
+    return redirect('index')
