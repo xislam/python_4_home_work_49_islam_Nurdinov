8ad3178bb76bf149fa8cb70d09322b0804a8b882
islam <nurdinovislam4@gmail.com>
2019-12-04 16:54:16 +0600

    дз 69 сделано

--- source/api_v2/serializers.py
+++ source/api_v2/serializers.py
@@ -2,19 +2,17 @@ from rest_framework import serializers
 from webapp.models import Project, IssueTracker
 
 
-class ProjectSerializer(serializers.ModelSerializer):
-
-    class Meta:
-
-        model = Project
-
-        fields = ('id', 'name_project', 'description_project', 'created_at', 'updated_at')
-
-
 class IssueSerializer(serializers.ModelSerializer):
 
     class Meta:
-
         model = IssueTracker
-
         fields = ('id', 'summary', 'description', 'status', 'type', 'project', 'date_ct', 'created_by', 'assigned_to')
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    issue_project = IssueSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Project
+        fields = ('id', 'name_project', 'description_project', 'created_at', 'updated_at', 'issue_project')
+
